1d072b9cd6370f64e12e1fef85b2e68067c1ff64
Brook Rodia <brookrodia@gmail.com>
2020-02-21 08:03:52 -0500

    create form view

--- app/views.py
+++ app/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic import CreateView
+from app.models import *
 
-class Hello(TemplateView):
+
+class Landing(TemplateView):
     template_name = 'index.html'
 
 
 class Contact(TemplateView):
     template_name = 'contact.html'
+
+
+class Form(CreateView):
+    model = Skillset
+    template_name = 'form.html'
+    fields = ['company_name', 'company_size', 'location',
+              'remote_work', 'language_requirement']
